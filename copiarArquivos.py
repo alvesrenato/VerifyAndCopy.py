793c42d99098d18346ab611f890d669ea6f7c2e6
Renato Alves <alves.renato@gmail.com>
2022-06-15 15:13:09 -0300

    le o arquivo, comprar e move para a pasta

--- copiarArquivos.py
+++ copiarArquivos.py
@@ -1,16 +1,44 @@
+from genericpath import exists
 import os
 import shutil
 
-caminhoOriginal = "Y:/7. RENATO ALVES/foto/CARTEIRINHA/teste"
-caminhoNovo = "Y:/7. RENATO ALVES/foto/CARTEIRINHA/teste/copia"
+meuArquivo = open('U:/PROJETOS/Rosário/2022/CARTEIRINHA PROVISÓRIA/15-06-22-PRODUÇÃO - CARTEIRINHA ESCOLAR - FUTUROESCOLAR/matriculas.txt','r')
+matriculas = meuArquivo.readlines()
 
-try: os.mkdir(caminhoNovo)
-except FileExistsError as e:
-    print("caminho {} já existe",format(caminhoNovo))
+# print(matriculas)
 
-for root, dirs, files in os.walk(caminhoOriginal):
-    for file in files:
-        caminhoAntigoPath = os.path.join(root, file)
-        caminhoNovoPath = os.path.join(caminhoNovo, file)
-        shutil.copy(caminhoAntigoPath, caminhoNovoPath)
+caminhoOriginal = 'Y:/7. RENATO ALVES/foto/CARTEIRINHA/'
+caminhoNovo = 'U:/PROJETOS/Rosário/2022/CARTEIRINHA PROVISÓRIA/15-06-22-PRODUÇÃO - CARTEIRINHA ESCOLAR - FUTUROESCOLAR/'
+
+for matricula in matriculas:
+    matricula = matricula.replace('\n','')
     
+    if os.path.exists(caminhoOriginal+matricula+'.jpg'):
+        shutil.copy(caminhoOriginal+matricula+'.jpg', caminhoNovo+matricula+'.jpg')
+    else:
+        print('Não foi: '+ matricula)
+
+
+
+# if os.path.exists(caminhoOriginal):
+#     print ("o arquivo existe")
+# else:
+#     print("o arquivo no ecxiste")
+        
+    
+
+
+# caminhoNovo = "Y:/7. RENATO ALVES/foto/CARTEIRINHA/teste/copia"
+
+# try: os.mkdir(caminhoNovo)
+# except FileExistsError as e:
+#     print("caminho {} já existe",format(caminhoNovo))
+
+# for root, dirs, files in os.walk(caminhoOriginal):
+#     for file in files:
+#         caminhoAntigoPath = os.path.join(root, file)
+#         caminhoNovoPath = os.path.join(caminhoNovo, file)
+#         shutil.copy(caminhoAntigoPath, caminhoNovoPath)
+        
+        
+        
